95ee31f33a230c716089265391b6ae1372d19e4f
Yung Chun Ern Martin <martinyce@gmial.com>
2016-08-19 13:33:18 +0800

    added search by location and language

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,12 @@ class TweetsController < ApplicationController
 	before_action :set_twitter_client
 
 	def get_tweets
-		@get_tweets = @twitter.search("die murder").take(3)
+		@get_tweets = @twitter.search("die suicide", geocode:"#{1.3521},#{103.8198},#{100}km", lang:"en").take(100)
 	end
 
 	def index
 		@tweets = get_tweets
 	end
-
 
 	private
 	def set_twitter_client
