065f58f08fa1eb4f5e61a5892e1d2be6a842c394
Yung Chun Ern Martin <martinyce@gmial.com>
2016-08-22 14:17:30 +0800

    integrated aylien, but slow, need optimise

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,14 @@
 class TweetsController < ApplicationController
-	before_action :set_twitter_client
+	before_action :set_twitter_client, :set_aylien_client
 
 	def get_tweets
 		set_user		
 		x = @twitter.user_timeline(@user.twitter_user_id.to_i)
 		last_id = x.last.id  
-		prev_id = 0
 		@tweets =[]
 		i =1
 		while i <= 30 do
 			@tweets << @twitter.user_timeline(@user.twitter_user_id.to_i , max_id: last_id)
-	    	prev_id = last_id
 	    	last_id = @tweets.last.last.id
 	    	i+=1
 		end
@@ -25,8 +23,11 @@ class TweetsController < ApplicationController
 	def create
 		set_user
 		@tweets = get_tweets
-		@tweets.each do |tweet|
-		@user.tweets.create(text: tweet.full_text, twitter_tweet_id: tweet.id)
+		@tweets.each do |outer|
+			outer.each do |tweet|
+				result = @aylien.sentiment text: "#{tweet.full_text}"
+				@user.tweets.create(text: tweet.full_text, twitter_tweet_id: tweet.id, polarity: result[:polarity], polarity_confidence: result[:polarity_confidence])
+			end
 		end
 		redirect_to '/'
 
@@ -46,6 +47,9 @@ class TweetsController < ApplicationController
 
 	end
 
+	def set_aylien_client
+		@aylien = AylienTextApi::Client.new
+	end
 
 	def set_twitter_client
 		@twitter = AngelAi::Application.config.twitter
---
+++ config/initializers/aylien.rb
@@ -0,0 +1,6 @@
+AylienTextApi.configure do |config|
+  config.app_id        =    ENV['AYLIEN_APP_ID']
+  config.app_key       =    ENV['AYLIEN_APP_KEY']
+end
+
+
