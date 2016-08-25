114f106a8b6c9f97c4ef132f69931b9bd8f8327e
Preethikasd <mspreethika@gmail.com>
2016-08-25 12:24:22 +0800

    created a login/signup page for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static#index'
+  	#root 'static#index'
+  	root 'admins#new'
+
+	resources :users
+		resources :sentiment_results, only: [:index, :create]
+	 	resources :filtered_results, only: [:index, :create]
 
 
-  resources :users
- 	resources :sentiment_results, only: [:index, :create]
- 	resources :filtered_results, only: [:index, :create]
+	resources :users do 
+	  	resources :tweets 
+	  		resources :sentiment_results, only: [:show,:destroy]
+	  			 resources :filtered_results, only: [:show, :destroy]
+	  end
 
-
-  resources :users do 
-  	resources :tweets 
-  		resources :sentiment_results, only: [:show,:destroy]
-  			 resources :filtered_results, only: [:show, :destroy]
-  end
-
- 
-
-
-
-  
-
-
+ 	resources :admins, only: [:new, :create]
+  	get "/get_user" => "static#index"
 
 end
 
