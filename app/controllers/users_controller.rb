dd69f8c4c72c3fe087ef0da309c34f0dc0501e85
Yung Chun Ern Martin <martinyce@gmial.com>
2016-08-25 11:07:20 +0800

    change user create action to the root page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,14 @@ class UsersController < ApplicationController
 	before_action :set_twitter_client
 
 	def get_user
-		@get_user = @twitter.search("isolating myself, i need to interact w humans again",lang:"en").take(1)
+		@get_user = @twitter.search("die suicide kill", geocode:"#{1.3521},#{103.8198},#{100}km", lang:"en").take(10)
 	end
 
 
 	def create
 		@users = get_user
 		@users.each do |user|
-			User.create(screen_name: user.user.screen_name, twitter_user_id: user.user.id)
+			User.find_or_create_by(screen_name: user.user.screen_name, twitter_user_id: user.user.id)
 		end
 		redirect_to users_path
 	end
