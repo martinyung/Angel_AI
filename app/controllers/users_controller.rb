28923158d24a69a8657dacef6bb3fe552c917f4e
Yung Chun Ern Martin <martinyce@gmial.com>
2016-08-25 16:59:21 +0800

    solved search key word function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,21 @@ class UsersController < ApplicationController
 	before_action :set_twitter_client
 
 	def create
-		@users = get_user
-		@users.each do |user|
-			User.find_or_create_by(screen_name: user.user.screen_name, twitter_user_id: user.user.id)
+		if params[:search].split.count > 0 and params[:search].split.count < 11
+			@users = @twitter.search(params[:search], lang:"en").take(2)
+			@users.each do |user|
+				User.find_or_create_by(screen_name: user.user.screen_name, twitter_user_id: user.user.id)
+			end
+			redirect_to users_path
+		else
+			@error = "Invalid search input, please try again"
+			render "static/index"
 		end
-		redirect_to users_path
+		
 	end
 
 	def index
-		@users = get_user
+		@users
 	end
 
 	def show 
@@ -19,12 +25,7 @@ class UsersController < ApplicationController
 
 	private
 
-	def get_user
-		@get_user = @twitter.search("kill suicide", lang:"en").take(5)
-	end
-
 	def set_twitter_client
 		@twitter = AngelAi::Application.config.twitter
 	end
-
 end
