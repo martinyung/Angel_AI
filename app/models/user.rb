c576f6718b10c0ea66c9effac108e9aa8d067cff
Yung Chun Ern Martin <martinyce@gmial.com>
2016-08-28 15:15:54 +0800

    indexing all foreign keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ApplicationRecord
 
 	has_many :tweets
 	has_one :filtered_result, :through => :tweet
-
-
 end
--- db/migrate/20160819061910_create_tweets.rb
+++ db/migrate/20160819061910_create_tweets.rb
@@ -1,10 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.0]
   def change
     create_table :tweets do |t|
-    	t.belongs_to :user
+    	t.belongs_to :user, index: true
     	t.string :text
     	t.string :twitter_tweet_id
-
       t.timestamps
     end
   end
